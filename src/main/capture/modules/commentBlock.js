431868190eb9771852f639e5c67ab4f79d5000fc
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-02-11 12:33:58 -0500

    fixed comment block capture

--- src/main/capture/modules/commentBlock.js
+++ src/main/capture/modules/commentBlock.js
@@ -1,15 +1,14 @@
 module.exports = function commentBlock(buffer) {
-  let capture = true;
   let i = 0;
-  let s = buffer.string.substring(0, i);
+  let s = buffer.string;
 
-  for (; capture && buffer.string[i]; i++) {
-    s += buffer.string[i];
-    if (s.substring(i, i + 2) === '*/') {
-      buffer.string = buffer.string.substring(i);
-      return {
-        value : s.split('\n'),
-      };
-    }
+  while (s.substring(i - 2, i) !== '*/' && s[i]) {
+    i += 1;
   }
+
+  buffer.string = s.substring(i);
+
+  return {
+    value : s.substring(2, i - 2).trim().split('\n'),
+  };
 };
